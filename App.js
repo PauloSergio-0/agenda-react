310533a0ece2bf0d7e97d8ae2d700aa1c3e35e09
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-09-23 20:19:40 -0300

    test

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { useFonts } from 'expo-font';
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Platform, StatusBar} from 'react-native';
 
 export default function App() {
 
@@ -17,24 +16,26 @@ export default function App() {
     if (!isFontsLoaded){
       return null;
     }
-    
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style= {{fontFamily:'GeneralSans-400'}}>Open up App.js to start working on your app!</Text>
       <Text style= {{fontFamily:'GeneralSans-600'}}>Open up App.js to start working on your app!</Text>
       <Text style= {{fontFamily:'GeneralSans-700'}}>Open up App.js to start working on your app!</Text>
       <Text style= {{fontFamily:'GeneralSans-700'}}>test</Text>
 
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight: 0,
+    // flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+
 });
