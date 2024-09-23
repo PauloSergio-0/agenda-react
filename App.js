98fd12694e751abfbff40a3b28f6f729f40170ae
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-09-23 20:00:30 -0300

    add fonts

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
+import { useFonts } from 'expo-font';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-
 export default function App() {
+
+
+    /* importando fonts*/
+  const [isFontsLoaded] = useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+
+    if (!isFontsLoaded){
+      return null;
+    }
+    
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style= {{fontFamily:'GeneralSans-400'}}>Open up App.js to start working on your app!</Text>
+      <Text style= {{fontFamily:'GeneralSans-600'}}>Open up App.js to start working on your app!</Text>
+      <Text style= {{fontFamily:'GeneralSans-700'}}>Open up App.js to start working on your app!</Text>
+      <Text style= {{fontFamily:'GeneralSans-700'}}>test</Text>
+
       <StatusBar style="auto" />
     </View>
   );
